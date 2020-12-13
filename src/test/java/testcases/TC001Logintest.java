d7772ce261ef1a9b403b85fc95c049daaaa4b5e6
Your Name <you@example.com>
2020-12-12 19:35:32 -0500

    first commit

---
+++ src/test/java/testcases/TC001Logintest.java
@@ -0,0 +1,35 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC001Logintest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vinay\\eclipse-workspace\\AutomationQA\\src\\test\\resources\\driverExecutables\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://zero.webappsecurity.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("signin_button")).click();
+		driver.findElement(By.id("user_login")).sendKeys("username");
+		driver.findElement(By.id("user_password")).sendKeys("password");
+		
+		
+		driver.findElement(By.name("submit")).click();
+		
+		driver.quit();
+
+
+	}
+
+}
